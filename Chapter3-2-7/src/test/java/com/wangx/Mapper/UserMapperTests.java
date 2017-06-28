78e52fab73a7c27045413a1f0efe4c9fce30f4b7
wx91 <766221790@qq.com>
2017-06-28 09:52:21 +0800

    加上事务的注解

--- Chapter3-2-7/src/test/java/com/wangx/Mapper/UserMapperTests.java
+++ Chapter3-2-7/src/test/java/com/wangx/Mapper/UserMapperTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.wangx.domain.User;
 import com.wangx.mapper.UserMapper;
@@ -15,14 +16,16 @@ import com.wangx.mapper.UserMapper;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @SpringBootApplication
+@Transactional
 public class UserMapperTests {
-	
+
 	@Autowired
 	private UserMapper userMapper;
+
 	@Test
 	@Rollback
-	public void findByName() throws Exception{
-//		userMapper.insert("AAA", 20);
+	public void findByName() throws Exception {
+		 userMapper.insert("AAA", 20);
 		User u = userMapper.findByName("AAA");
 		Assert.assertEquals(20, u.getAge().intValue());
 	}
