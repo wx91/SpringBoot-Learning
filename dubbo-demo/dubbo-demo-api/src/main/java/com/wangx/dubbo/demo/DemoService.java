195ce1d434e64c6368b27f20bf0ef3d602b3422e
wx91 <766221790@qq.com>
2017-12-25 16:56:17 +0800

    1.删除sharding-jdbc的例子。增加dubbo的例子

---
+++ dubbo-demo/dubbo-demo-api/src/main/java/com/wangx/dubbo/demo/DemoService.java
@@ -0,0 +1,6 @@
+package com.wangx.dubbo.demo;
+
+public interface DemoService {
+
+	String sayHello(String name);
+}
---
+++ dubbo-demo/dubbo-demo-provider/src/main/java/com/wangx/dubbo/demo/provider/DemoServiceImpl.java
@@ -0,0 +1,15 @@
+package com.wangx.dubbo.demo.provider;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.wangx.dubbo.demo.DemoService;
+
+public class DemoServiceImpl implements DemoService {
+	public String sayHello(String name) {
+		System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name
+				+ ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
+		return "Hello " + name + ", response form provider: " + RpcContext.getContext().getLocalAddress();
+	}
+}
